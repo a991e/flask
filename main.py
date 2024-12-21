70efc9979ff4e50095c8255bbf5ab34e70a9c0da
a991e <apple@027168.com>
2024-12-21 20:26:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 import requests
 from urllib.parse import urlparse
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'Hello World'
+    return 'Hello World'
 
-@app.route('/f/<path:url>')
-def forward(url):
-   if url.find('http:') != -1:
-      url = url[0:6] + '/' + url[6:]
-   elif url.find('https:') != -1:
-      url = url[0:7] + '/' + url[7:]
-   else:
-      url = "https://" + url
-   parsed_url = urlparse(url)
-   referer = parsed_url.scheme + "://" + parsed_url.netloc
-   headers = {
+@app.route('/f')
+def forward():
+    url = request.args.get('url')
+    parsed_url = urlparse(url)
+    referer = parsed_url.scheme + "://" + parsed_url.netloc
+    headers = {
         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
         "Referer":referer,
-   }
-   try:
-      response = requests.get(url,headers=headers)
-   except Exception as e:
-      return e
-   return (response.content, response.status_code, response.headers.items())
-
+    }
+    try:
+        response = requests.get(url,headers=headers,timeout=15)
+    except Exception as e:
+        return '[-] can not fetch image!'
+    return (response.content, response.status_code, response.headers.items())
 if __name__ == '__main__':
-   app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
